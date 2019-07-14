1db43772f2fc5225aedcc0290be498d414cd6616
marinatuji <mtujimura@gmail.com>
2019-07-14 17:39:34 -0300

    Refatoração:Suite de testes e melhoria de código do index.js

--- card-validator-lib/lib/index.js
+++ card-validator-lib/lib/index.js
@@ -1,14 +1,14 @@
 function cardValidator(card) {
-  const inputCard = convertInputToArrayString(card);
-  const arrayCard = parseToNumber(inputCard);
-  const luhnNumber = calcLuhn(arrayCard);
-  if (((10 - (luhnNumber % 10)) === arrayCard[arrayCard.length - 1])) {
+  const cardString = parseString(card);
+  const cardNumber = parseNumber(cardString);
+  const luhnNumber = calculateLuhn(cardNumber);
+  if (((10 - (luhnNumber % 10)) === cardNumber[cardNumber.length - 1])) {
     return true;
   }
   return false;
 }
 
-function convertInputToArrayString(card) {
+function parseString(card) {
   if (typeof card === 'string') {
     return card.replace(/\D/g, '').split('');
   } else if (typeof card === 'number') {
@@ -17,7 +17,7 @@ function convertInputToArrayString(card) {
   throw new Error(`Tipo de entrada inválida: ${typeof card}`);
 }
 
-function parseToNumber(arrayCard) {
+function parseNumber(arrayCard) {
   if (arrayCard.length === 16) {
     return arrayCard
       .map(Number);
@@ -25,11 +25,11 @@ function parseToNumber(arrayCard) {
   throw new Error(`Tamanho da entrada inválida: ${arrayCard.length}`);
 }
 
-function calcLuhn(arrayCard) {
+function calculateLuhn(arrayCard) {
   return arrayCard
     .slice(0, arrayCard.length - 1)
     .map((currentValue, index) => (index % 2 === 0 ? currentValue * 2 : currentValue))
-    .map((currentValue, index) => ((currentValue > 9) ? (currentValue - 9) : currentValue)) 
+    .map((currentValue) => ((currentValue > 9) ? (currentValue - 9) : currentValue)) 
     .reduce((acc, currentValue) => acc += currentValue);
 }
 
--- card-validator-lib/test/index.spec.js
+++ card-validator-lib/test/index.spec.js
@@ -1,28 +1,37 @@
 const expect = require('chai').expect;
 const cardValidator = require('../lib/index');
 
-describe('cardValidator()', () => {
-  describe('Número de cartão válido:', () => {
-    it('Retorna false: Entrada válida menor que 16 digitos', () => {
-      expect(cardValidator(8532)).to.equal(false);
-    });
-    it('Retorna true: Entrada válida de 16 dígitos', () => {
+describe('Suite de testes para cardValidator()', () => {
+  describe('Cenários de entradas válidas:', () => {
+    it('Cartão válido de 16 dígitos como números', () => {
       expect(cardValidator(4984235062514488)).to.equal(true);
     });
-    it('Retorna true: Entrada válida como string', () => {
+    it('Cartão válido como string', () => {
       expect(cardValidator('4984235062514488')).to.equal(true);
     });
-  });
-  
+    it('Cartão válida com máscara', () => {
+      expect(cardValidator('4984 2350 6251 4488')).to.equal(true);
+    });
+  })
+
   describe('Número de cartão inválido:', () => {
-    it('Retorna false: Cartão inválido', () => {
+    it('Cartão inválido com máscara', () => {
       expect(cardValidator('4984 2350 6251 4489')).to.equal(false);
     });
-    it('Retorna false: Entrada inválida (menor que 16 dígitos)', () => {
-      expect(cardValidator(49842350)).to.equal(false);
+    it('Cartão inválido como número', () => {
+      expect(cardValidator(4984235062514489)).to.equal(false);
     });
-    it('Retorna false: Entrada inválida (tipo booleano)', () => {
-      expect(cardValidator(true)).to.equal(false);
+  });
+
+  describe('Cenário de mensagens de erro:', () => {
+    it('Tipo de entrada inválida (booleano)', () => {
+      expect(() => cardValidator(true)).to.throw('Tipo de entrada inválida: boolean');
+    });
+    it('Entrada válida menor que 16 digitos como número', () => {
+      expect(() => cardValidator(8532)).to.throw('Tamanho da entrada inválida: 4');
+    });
+    it('Entrada válida menor que 16 digitos como string', () => {
+      expect(() => cardValidator('8532')).to.throw('Tamanho da entrada inválida: 4');
     });
   });
 })
